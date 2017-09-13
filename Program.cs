4d0b0a52f8b00ce99a835f6d977cced8b6dfd614
Iftikhar Ali <iftikharzulfiqar6@gmail.com>
2017-09-13 16:49:00 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 10; i = i + 2)
+            {
+                for (int k = 9; k >= i; k = k - 2)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = 0; j < i; j++)
+                {
+
+                    Console.Write("*");
+
+                }
+
+                Console.Write("\n");
+            }
+            Console.ReadLine();
+        }
+    }
+}
